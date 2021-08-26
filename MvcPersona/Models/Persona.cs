46e844492c1371776ecbde4638f81406f382d89b
cmerin0 <carlosmerino@gmail.com>
2021-08-26 17:22:07 -0600

    agregando todo el proyecto persona con su buscador

---
+++ MvcPersona/Models/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcPersona.Models
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+    public class PersonaDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
